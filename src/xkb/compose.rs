a200b699436bd00882cf5c827c8fc659123c0122
Lucius Hu <lebensterben@users.noreply.github.com>
2021-08-11 01:02:02 -0400

    compose: Added {get,from}-raw-pointer

--- src/xkb/compose.rs
+++ src/xkb/compose.rs
@@ -108,6 +108,16 @@ pub struct State {
 }
 
 impl State {
+    pub unsafe fn from_raw_ptr(ptr: *mut xkb_compose_state) -> State {
+        State {
+            ptr: ptr
+        }
+    }
+
+    pub fn get_raw_ptr(&self) -> *mut xkb_compose_state {
+        self.ptr
+    }
+
     #[must_use]
     pub fn new(table: &Table, flags: StateFlags) -> State {
         State {
