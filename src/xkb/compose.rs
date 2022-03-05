4858695068f6d79cd48880ccdb8b2e40e6faf38e
Remi Thebault <remi.thebault@gmail.com>
2022-03-05 22:38:12 +0100

    cargo fmt

--- src/xkb/compose.rs
+++ src/xkb/compose.rs
@@ -109,9 +109,7 @@ pub struct State {
 
 impl State {
     pub unsafe fn from_raw_ptr(ptr: *mut xkb_compose_state) -> State {
-        State {
-            ptr: ptr
-        }
+        State { ptr: ptr }
     }
 
     pub fn get_raw_ptr(&self) -> *mut xkb_compose_state {
