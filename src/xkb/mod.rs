6c4658dc8e9d5336b070821fb653e4f8387b7377
Remi Thebault <remi.thebault@gmail.com>
2022-03-05 23:05:21 +0100

    cargo fmt

--- src/xkb/mod.rs
+++ src/xkb/mod.rs
@@ -721,13 +721,8 @@ impl Keymap {
             // Starting in version 7 of the wl_keyboard protocol, the keymap must be mapped using MAP_PRIVATE.
             .map_copy_read_only(&fs::File::from_raw_fd(fd))
             .unwrap();
-        let ptr = xkb_keymap_new_from_buffer(
-            context.ptr,
-            map.as_ptr().cast(),
-            size - 1,
-            format,
-            flags,
-        );
+        let ptr =
+            xkb_keymap_new_from_buffer(context.ptr, map.as_ptr().cast(), size - 1, format, flags);
         if ptr.is_null() {
             None
         } else {
